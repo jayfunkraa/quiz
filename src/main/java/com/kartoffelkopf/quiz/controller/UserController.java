99552d000491545042a44b0876ea5d5e115352c8
Jamie <jay.hanna@outlook.com>
2020-04-10 17:38:46 +0100

    Security completed

--- src/main/java/com/kartoffelkopf/quiz/controller/UserController.java
+++ src/main/java/com/kartoffelkopf/quiz/controller/UserController.java
@@ -4,12 +4,20 @@ import com.kartoffelkopf.quiz.model.User;
 import com.kartoffelkopf.quiz.service.RoleService;
 import com.kartoffelkopf.quiz.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
@@ -37,5 +45,37 @@ public class UserController {
         user.setRole(roleService.getDefaultRole());
         userService.save(user);
         return "redirect:/login";
+    }
+
+    @RequestMapping("/user/edit")
+    private String editForm(Model model, Principal principal) {
+        User user = (User)((UsernamePasswordAuthenticationToken)principal).getPrincipal();
+        model.addAttribute("user", user);
+        return "user/edit";
+    }
+
+    @RequestMapping(value = "/user/save", method = RequestMethod.POST)
+    public String edit(@RequestParam String username,
+                       @RequestParam String firstName,
+                       @RequestParam String lastName,
+                       @RequestParam String password,
+                       @RequestParam String confirmPassword,
+                       Principal principal) {
+        User user = userService.findByUsername(username);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        if (!password.isEmpty()) {
+            String passwordHash = passwordEncoder.encode(password);
+            user.setPassword(passwordHash);
+        }
+        user.setEnabled(true);
+        user.setRole(roleService.getDefaultRole());
+        userService.save(user);
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(user, user.getPassword(),
+                user.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return "redirect:/";
     }
 }
